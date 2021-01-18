e265820cb537070e1ce8fdea0531a9199a687ebb
Candra <candraherdiansyah14@gmail.com>
2021-01-18 09:44:32 +0700

    auth

---
+++ resources/views/latihan2.blade.php
@@ -0,0 +1,4 @@
+@extends('layouts.master')
+@section('content')
+Hallo Lagi
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,20 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+// template
+Route::get('practice',function()
+{
+    return view('latihan');
+});
+
+Route::get('latihan',function()
+{
+    return view('latihan2');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
