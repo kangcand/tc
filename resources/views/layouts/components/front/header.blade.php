3868fc49c3a6d45e5c52d07c4ca992e2af518ae0
Candra <candraherdiansyah14@gmail.com>
2021-02-24 15:23:22 +0700

    github

--- resources/views/layouts/components/front/header.blade.php
+++ resources/views/layouts/components/front/header.blade.php
@@ -3,7 +3,7 @@
         <li>
             <img src="{{asset('assets/covid.png')}}" style="width: 65px; height:65px; margin-left:25px;margin-right:15px; margin-bottom:15px; margin-top:15px;" alt="">
         </li>
-        <li class="c-header-nav-item px-3"><a class="c-header-nav-link" href="#" style="font-size: 20px;">Dashboard</a></li>
+        <li class="c-header-nav-item px-3"><a class="c-header-nav-link" href="#" style="font-size: 20px;">Beranda</a></li>
         <li class="c-header-nav-item px-3"><a class="c-header-nav-link" href="#" style="font-size: 20px;">About </a></li>
         <li class="c-header-nav-item px-3"><a class="c-header-nav-link" href="#" style="font-size: 20px;">Kontak</a></li>
     </ul>
