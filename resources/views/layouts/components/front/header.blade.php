5134e6bad24f203b07cf8547846b95eb4c9f3de7
Candra <candraherdiansyah14@gmail.com>
2021-02-24 15:26:02 +0700

    rubah nav

--- resources/views/layouts/components/front/header.blade.php
+++ resources/views/layouts/components/front/header.blade.php
@@ -1,11 +1,16 @@
 <header class="c-header c-header-light c-header-fixed c-header-with-subheader">
     <ul class="c-header-nav d-md-down-none" style="">
         <li>
-            <img src="{{asset('assets/covid.png')}}" style="width: 65px; height:65px; margin-left:25px;margin-right:15px; margin-bottom:15px; margin-top:15px;" alt="">
+            <img src="{{ asset('assets/covid.png') }}"
+                style="width: 65px; height:65px; margin-left:25px;margin-right:15px; margin-bottom:15px; margin-top:15px;"
+                alt="">
         </li>
-        <li class="c-header-nav-item px-3"><a class="c-header-nav-link" href="#" style="font-size: 20px;">Beranda</a></li>
-        <li class="c-header-nav-item px-3"><a class="c-header-nav-link" href="#" style="font-size: 20px;">About </a></li>
-        <li class="c-header-nav-item px-3"><a class="c-header-nav-link" href="#" style="font-size: 20px;">Kontak</a></li>
+        <li class="c-header-nav-item px-3"><a class="c-header-nav-link" href="#" style="font-size: 20px;">Beranda</a>
+        </li>
+        <li class="c-header-nav-item px-3"><a class="c-header-nav-link" href="#" style="font-size: 20px;">Tentang </a>
+        </li>
+        <li class="c-header-nav-item px-3"><a class="c-header-nav-link" href="#" style="font-size: 20px;">Kontak</a>
+        </li>
     </ul>
 
 </header>
